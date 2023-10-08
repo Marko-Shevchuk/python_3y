e402ee4887d40ace79f3a84053f9d4d68a3e00dd
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2023-10-09 00:08:16 +0300

    Added support for multiple messages per client

--- client.py
+++ client.py
@@ -3,9 +3,13 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('127.0.0.1', 12345)
 server_socket.connect(server_address)
-message = input("SPEAK. ") 
-server_socket.send(message.encode('utf-8')) 
-server_socket.send(str(len(message)).encode('utf-8')) 
-response = server_socket.recv(1024).decode('utf-8')
-print(response)
+while True:
+    message = input("SPEAK. ") 
+    server_socket.send(message.encode('utf-8')) 
+    server_socket.send(str(len(message)).encode('utf-8')) 
+    if message.lower() == "exit":
+        break
+    response = server_socket.recv(1024).decode('utf-8')
+    print(response)
+    
 server_socket.close()
