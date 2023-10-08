9dfa4e2a2c761ba5f819342d1f0513fa73025f40
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2023-10-09 01:44:54 +0300

    Fixed folder path

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_address = ('127.0.0.1', 12345)
-client_socket.connect(server_address)
-message = input("SPEAK. ") 
-client_socket.send(message.encode('utf-8')) 
-
-client_socket.close()
